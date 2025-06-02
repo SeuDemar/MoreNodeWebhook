c5c838dd4a587d79abce4e0598479fb770ecf2bd
SeuDemar <seudemar@outlook.com>
2025-06-02 01:48:35 -0300

    Adicionando JWT

---
+++ middlewares/authenticateToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT_SECRET || 'sua_chave_secreta';
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']; // Espera algo como 'Bearer token_aqui'
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.status(401).json({ error: 'Token não fornecido' });
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.status(403).json({ error: 'Token inválido' });
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authenticateToken;
